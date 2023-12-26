a14ac97672c88a210cbd11007eeadd85fcb27fa2
Dmitry <chernyshov85@mail.ru>
2023-12-26 20:06:38 +0300

    sem_2

--- main.py
+++ main.py
@@ -1,16 +1,51 @@
-# This is a sample Python script.
+from flask import Flask, render_template, request, redirect, url_for, session, make_response
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+app = Flask(__name__)
 
 
-# Press the green button in the gutter to run the script.
+app.secret_key = 'f3c26108e4e0747b5bada82bc68cf60c8ff47ab27974fca8693ccd00043a4f68'
+
+@app.route('/')
+def index():
+    if 'login' in session:
+        return f'Привет, {session["login"]}'
+    else:
+        return redirect(url_for('submit'))
+
+
+
+@app.get('/submit/')
+def submit_get():
+    context = {
+        'login': 'Авторизация'
+    }
+    return render_template('form.html', **context)
+
+
+@app.post('/submit/')
+def submit_post():
+    session['login'] = request.form.get('login')
+    session['email'] = request.form.get('email')
+    return redirect(url_for('success'))
+
+
+@app.route('/success/', methods=['GET', 'POST'])
+def success():
+    if 'login' in session:
+        context = {
+            'login': session['login'],
+            'email': session['email'],
+            'title': 'Добро пожаловать'
+        }
+        if request.method == 'POST':
+            session.pop('login', None)
+            session.pop('email', None)
+            return redirect(url_for('submit_get'))
+        return render_template('success.html', **context)
+    else:
+        return redirect(url_for('login'))
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    app.run(debug=True)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
